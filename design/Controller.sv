04f00f95865d42a1ae3373935fa2b2f4ebd6e291
Rodrigo Pontes <78002168+LatchedMicrobe@users.noreply.github.com>
2023-08-25 12:34:32 -0300

    Update Controller.sv

--- design/Controller.sv
+++ design/Controller.sv
@@ -17,10 +17,11 @@ module Controller (
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
     output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
     output logic Branch,  //0: branch is not taken; 1: branch is taken
-    output logic [1:0] RWSel
+    output logic [1:0] RWSel,
+    output logic jalrsel
 );
 
-  logic [6:0] R_TYPE, I_TYPE , U_TYPE, LW, SW, BR, JAL;
+  logic [6:0] R_TYPE, I_TYPE , U_TYPE, LW, SW, BR, JAL, JALR;
 
   assign R_TYPE = 7'b0110011;  //add,sub,and,or,xor
   assign I_TYPE = 7'b0010011; //slti,slli,srli,srai,addi
@@ -29,15 +30,17 @@ module Controller (
   assign SW = 7'b0100011;  //sw,sb,sh
   assign BR = 7'b1100011;  //beq,bne,bge,blt
   assign JAL = 7'b1101111; //jal
+  assign JALR = 7'b1100111; //jalr
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE || Opcode == U_TYPE);
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JALR);
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JAL);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JAL || Opcode == JALR);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
   assign ALUOp[0] = (Opcode == BR || Opcode == U_TYPE || Opcode == JAL);
-  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JAL);
-  assign Branch = (Opcode == BR || Opcode == JAL);
-  assign RWSel[0] = (Opcode == JAL);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JAL || Opcode == JALR);
+  assign Branch = (Opcode == BR || Opcode == JAL || Opcode == JALR);
+  assign RWSel[0] = (Opcode == JAL || Opcode == JALR);
   assign RWSel[1] = 0;
+  assign jalrsel = (Opcode == JALR);
 endmodule
